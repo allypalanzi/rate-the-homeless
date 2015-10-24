c77c74a013b192e5aedd9066dc8eac0dbd2eca07
Ally Palanzi <ampalanzi@gmail.com>
2015-10-24 14:26:12 -0400

    more stuff

--- js/main.js
+++ js/main.js
@@ -21,15 +21,15 @@ function marker (info, lat, lng) {
       '</div>'+
       '<h1 id="firstHeading" class="firstHeading">Joe Whatever</h1>'+
       '<div id="bodyContent">'+
-      '<a href="profile.html" > <img src="images/hipster.jpg" height="120" width="120"></a>'  +
+      '<a href="homeless.html" > <img src="images/hipster.jpg" height="120" width="120"></a>'  +
       '<p></p>'+
       '<p>Lorem ipsum dolor sit</p>' +
       '<p> amet, consectetur adipisicing </p>' +
       '<p> elit, sed do eiusmod tempor </p>' +
-      '<p>incididunt ut labore et dolore </p>' + 
+      '<p>incididunt ut labore et dolore </p>' +
       'magna aliqua. Ut enim ad minim</p>' +
       '<p> veniam, quis nostrud exercitation</p>'+
-      '<p> ullamco laboris nisi ut aliquip</p>' + 
+      '<p> ullamco laboris nisi ut aliquip</p>' +
       '<p> ex ea commodo consequat. </p>'+
       '<p> Duis aute irure dolor in</p>'+
       '<p> reprehenderit in voluptate </p>'+
@@ -49,11 +49,11 @@ function marker (info, lat, lng) {
     position: new google.maps.LatLng(38.905971, -77.0432083),
     map: map,
     title: 'Joe Whatever'
- 
+
   });
   marker.addListener('click', function() {
     infowindow.open(map, marker);
-  });  
+  });
 
 	infowindow.open(map,mark);
 
